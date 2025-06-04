c95cc3fcb7c10e139bc5ae94a220d0c9ea4c0f8d
AriaOrAries <91868169+AriaRGB@users.noreply.github.com>
2025-06-04 18:03:45 -0400

    transition def beats out animation when i comes to small stuff
    Fine Again - Seether

---
+++ script.js
@@ -0,0 +1,7 @@
+projectmouse = document.getElementById("projectanchor")
+aboutmouse = document.getElementById("aboutanchor")
+creditmouse = document.getElementById("creditsanchor")
+
+projectmenu = document.getElementById("ProjectMenu")
+aboutmenu = document.getElementById("AboutMe")
+creditmenu = document.getElementById("Credits")
